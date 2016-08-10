e63ca418134b27fa0b5f08b8ec784b1b5605c3c7
OJBlack <osmarfsjunior@outlook.com>
2016-08-09 22:49:27 -0300

    Day_01_Hello_World
    Projeto 30 dias de Code

---
+++ Day_01_Hello_World/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Day_01_Hello_World
+{
+    class Solution
+    {
+        static void Main(string[] args)
+        {
+            String inputString; // declare a variable to hold our input
+            inputString = Console.ReadLine(); // get a line of input from stdin and save it to our variable
+
+            // Your first line of output goes here
+            Console.WriteLine("Hello, World.");
+
+            // Write the second line of output
+            Console.Write(inputString);
+        }
+    }
+}

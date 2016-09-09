c0d0b6f900150926c0592cf000b9d0a6be646310
Mr. OJ <oj13black@hotmail.com>
2016-09-09 16:20:54 -0700

    Demonstração - Finish

---
+++ Demonstracoes/Scripts/webWorker.js
@@ -0,0 +1,27 @@
+﻿self.addEventListener('message', function (event) {
+    var obj = JSON.parse(event.data)
+    var msg;
+    switch (parseInt(obj.comando)) {
+        case 1:
+            msg = 'Estou fazendo o backup.<br/>';
+            break;
+        case 2:
+            msg = 'Estou Executando o calculo Matemático.<br/>';
+            break;
+        case 3:
+            msg = 'Estou Copiando um Arquivo.<br/>';
+            break;
+        case 4:
+            msg = 'Finalizando.<br/>';
+            break;
+        default:
+
+    }
+    msg += 'A mensagem foi: ' + obj.msg;
+
+    self.postMessage(msg);
+
+    if (parseInt(obj.comando) == 4) {
+        self.close();
+    }
+});

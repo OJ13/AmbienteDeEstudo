3ddafc1b2a423cedfef54cb815e5e6cf81e83bc1
Mr. OJ <oj13black@hotmail.com>
2016-11-23 15:41:48 -0200

    Projeto Curso Padrão de Projeto

---
+++ Projeto_Curso_PadraoProjeto/Aula_01_ClassePOO/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Aula_01_ClassePOO
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int x = 10;
+            int y = 20;
+            int r = x + y;  //business
+
+            Console.WriteLine("A soma de {0} com {1} é {2}", x, y, r);
+
+            Console.ReadKey();
+        }
+    }
+}
